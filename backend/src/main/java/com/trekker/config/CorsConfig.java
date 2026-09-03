fc071fb557f66321d91b55fd8045e4d425b893ea
Suryansh Sharma <suryansharmaa@gmail.com>
2026-09-03 21:40:15 +0530

    Fix CORS for frontend

--- backend/src/main/java/com/trekker/config/CorsConfig.java
+++ backend/src/main/java/com/trekker/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("https://thetrekker.netlify.app", "http://localhost:5173")
+                        .allowedOrigins("https://trekker.netlify.app", "http://localhost:5173")
                         .allowedMethods("GET", "POST")
                         .allowCredentials(true);
             }
